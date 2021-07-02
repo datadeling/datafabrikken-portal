b7471ee7e582a2d2fa72c1a7bf48e5d73d6f3c53
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-07-02 08:17:13 +0200

    style(filters): minor change mobile view

--- src/entrypoints/main/router/pages/datasets-page/components/filters/styled.ts
+++ src/entrypoints/main/router/pages/datasets-page/components/filters/styled.ts
@@ -2,13 +2,21 @@ import styled from 'styled-components';
 
 import { theme } from '../../../../../app/theme';
 
+const onMobileView = '@media (max-width: 900px)';
+
 const Title = styled.div`
   font-size: ${theme.fontSize('FS16')};
   margin-bottom: ${theme.spacing('S16')};
+  ${onMobileView} {
+    display: none;
+  }
 `;
 
 const FilterSection = styled.section`
   margin-bottom: ${theme.spacing('S20')};
+  ${onMobileView} {
+    margin-top: ${theme.spacing('S10')};
+  }
 `;
 
 const FilterTypeTitle = styled.div`
