3c6e622062845036e1b5f8ef56986a2200a04d46
Jeff Reiffers <jeffreiffers@gmail.com>
2021-10-14 18:10:35 +0200

    chore: integrate community google analytics

--- src/components/analytics/index.tsx
+++ src/components/analytics/index.tsx
@@ -4,25 +4,37 @@ import { compose } from 'redux';
 import ReactGA from 'react-ga4';
 
 import { GoogleAnalyticsTrackingId } from '../../types/enums';
+import { PATHNAME } from '../../enums';
 
 const Analytics: FC = () => {
   const { pathname, search } = useLocation();
   const { hostname } = location;
 
   const isDatafabrikken = ['datafabrikken.norge.no'].includes(hostname);
+  const isCommunity = [`${PATHNAME.COMMUNITY}`].includes(pathname);
 
   const configureAnalytics = () => {
+    const trackingIds = [];
     if (isDatafabrikken) {
-      ReactGA.initialize(GoogleAnalyticsTrackingId.DATAFABRIKKEN);
+      trackingIds.push(GoogleAnalyticsTrackingId.DATAFABRIKKEN);
+    }
+    if (isCommunity) {
+      trackingIds.push(GoogleAnalyticsTrackingId.COMMUNITY);
     }
 
-    ReactGA.set({ anonymizeIp: true });
+    if (trackingIds.length > 0) {
+      ReactGA.initialize(
+        trackingIds.map(trackingId => ({
+          trackingId
+        })),
+        { gaOptions: { anonymizeIp: true } }
+      );
+    }
   };
 
   const registerPageView = () => {
     if (isDatafabrikken) {
       const page = `${pathname}${search}`;
-
       ReactGA.set({ page });
       ReactGA.send({ hitType: 'pageview', page });
     }
