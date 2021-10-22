86641f6e2c5df5f167da6ca7811f389d106cc2ea
Jeff Reiffers <jeffreiffers@gmail.com>
2021-10-22 09:42:20 +0200

    chore: community topics HOC

--- src/entrypoints/main/redux/reducer.ts
+++ src/entrypoints/main/redux/reducer.ts
@@ -7,6 +7,7 @@ import CommunityCategoryReducer from '../../../components/with-community-categor
 import CommunityTagsReducer from '../../../components/with-community-tags/redux/reducer';
 import CommunityTopicReducer from '../../../components/with-community-topic/redux/reducer';
 import CommunitySearchReducer from '../../../components/with-community-search/redux/reducer';
+import CommunityTopicsReducer from '../../../components/with-community-topics/redux/reducer';
 import DatasetsReducer from '../../../components/with-datasets/redux/reducer';
 import ReferenceDataReducer from '../../../components/with-reference-data/redux/reducer';
 import DatasetReducer from '../../../components/with-dataset/redux/reducer';
@@ -22,6 +23,7 @@ export default combineReducers({
   CommunityTagsReducer,
   CommunityTopicReducer,
   CommunitySearchReducer,
+  CommunityTopicsReducer,
   DatasetsReducer,
   ReferenceDataReducer,
   DatasetReducer,
