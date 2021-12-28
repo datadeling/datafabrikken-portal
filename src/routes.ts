bd74bb5cd23d849c7fe4af3d84c391d1895be417
Jeff Reiffers <jeffreiffers@gmail.com>
2021-12-28 11:59:45 +0100

    chore: generate sitemap and add robots.txt

---
+++ src/routes.ts
@@ -0,0 +1,34 @@
+import { PATHNAME } from './enums';
+
+export default {
+  [PATHNAME.MAIN]: 'main-page',
+  [PATHNAME.ABOUT]: 'article-page',
+  [PATHNAME.FIND_DATA]: 'datasets-page',
+  [`${PATHNAME.FIND_DATA}${PATHNAME.DATA_SOURCES}`]: 'article-page',
+  [`${PATHNAME.FIND_DATA}${PATHNAME.DATA_SOURCES}${PATHNAME.PRIVATE_SECTOR}`]:
+    'article-page',
+  [`${PATHNAME.FIND_DATA}${PATHNAME.GUIDEANCE_AND_COMPETENCE}`]: 'article-page',
+  [`${PATHNAME.FIND_DATA}${PATHNAME.GUIDEANCE_AND_COMPETENCE}${PATHNAME.LEGAL_FRAMEWORK}`]:
+    'article-page',
+  [`${PATHNAME.FIND_DATA}${PATHNAME.DATASET_DETAILS}/:datasetId`]:
+    'details-page-dataset',
+  [PATHNAME.NEWS]: 'news-page',
+  [`${PATHNAME.NEWS}/:newsArticleId`]: 'news-article-page',
+  [PATHNAME.COMMUNITY]: 'community-categories-page',
+  [PATHNAME.COMMUNITY_TAGS]: 'community-tags-page',
+  [PATHNAME.COMMUNITY_ABOUT]: 'article-page',
+  [`${PATHNAME.COMMUNITY}/:categorySlugId/:categorySlugTitle`]:
+    'community-topics-page',
+  [PATHNAME.COMMUNITY_POPULAR]: 'community-topics-page',
+  [PATHNAME.COMMUNITY_RECENT]: 'community-topics-page',
+  [`${PATHNAME.COMMUNITY_TAGS}/:tag`]: 'community-topics-page',
+  [`${PATHNAME.COMMUNITY}/:categorySlugId/:categorySlugTitle/:topicSlugId/:topicSlugTitle`]:
+    'community-topic-page',
+  [PATHNAME.CONTACT]: 'contact-page',
+  [PATHNAME.ORGANIZATION]: 'organization-page',
+  [`${PATHNAME.ORGANIZATION}/:organizationId${PATHNAME.METADATAQUALITY}`]:
+    'metadata-quality-datasets-page',
+  [`${PATHNAME.ORGANIZATION}/:organizationId${PATHNAME.METADATAQUALITY}/:datasetId`]:
+    'metadata-quality-dataset-page',
+  [PATHNAME.COURSES]: 'courses-page'
+};
