3fa58e861afb7a797bc46588df8c0849568c329c
Jeff Reiffers <jeffreiffers@gmail.com>
2022-03-02 09:45:56 +0100

    chore: design - h1-h6 use regular font weight 400

--- src/entrypoints/main/app/styles/fonts.ts
+++ src/entrypoints/main/app/styles/fonts.ts
@@ -13,4 +13,13 @@ export default css`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 400;
+  }
 `;
