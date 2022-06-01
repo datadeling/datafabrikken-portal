7003597701eb1c8e3e47e5b3377608f065191a9c
Jeff Reiffers <jeffreiffers@gmail.com>
2022-06-01 13:46:03 +0200

    feat: custom ga4 events for filter-search

--- src/entrypoints/main/router/routes/index.tsx
+++ src/entrypoints/main/router/routes/index.tsx
@@ -1,22 +1,31 @@
-import React, { memo, FC, lazy } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { memo, FC, lazy, useEffect } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ErrorPage from '../../../../components/error-page';
 import routes from '../../../../routes';
+import { registerPageView } from '../../../../utils/analytics';
 
-const Routes: FC = () => (
-  <Switch>
-    {Object.keys(routes)
-      .filter((path: string) => !!routes[path])
-      .map((path: string, index) => (
-        <Route
-          key={`${path}-${index}`}
-          exact
-          path={path}
-          component={lazy(() => import(`../pages/${routes[path]}`))}
-        />
-      ))}
-    <Route render={() => <ErrorPage errorCode='404' />} />
-  </Switch>
-);
+const Routes: FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    registerPageView(location.pathname);
+  }, [location.pathname]);
+
+  return (
+    <Switch>
+      {Object.keys(routes)
+        .filter((path: string) => !!routes[path])
+        .map((path: string, index) => (
+          <Route
+            key={`${path}-${index}`}
+            exact
+            path={path}
+            component={lazy(() => import(`../pages/${routes[path]}`))}
+          />
+        ))}
+      <Route render={() => <ErrorPage errorCode='404' />} />
+    </Switch>
+  );
+};
 
 export default memo(Routes);
---
+++ src/utils/analytics/index.ts
@@ -0,0 +1,46 @@
+import ReactGA from 'react-ga4';
+
+import { GoogleAnalyticsTrackingId } from '../../types/enums';
+import { PATHNAME } from '../../enums';
+
+export const initAnalytics = () => {
+  if (!ReactGA.isInitialized && typeof window !== 'undefined') {
+    const isCommunity = window.location.pathname.includes(PATHNAME.COMMUNITY);
+
+    const trackingIds = [GoogleAnalyticsTrackingId.DATAFABRIKKEN];
+    if (isCommunity) {
+      trackingIds.push(GoogleAnalyticsTrackingId.COMMUNITY);
+    }
+
+    if (trackingIds.length > 0) {
+      try {
+        ReactGA.initialize(
+          trackingIds.map(trackingId => ({
+            trackingId
+          })),
+          {
+            testMode: window.location.hostname !== 'datafabrikken.norge.no',
+            gaOptions: { anonymizeIp: true }
+          }
+        );
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      }
+    }
+  }
+};
+
+export const registerPageView = (page: string) => {
+  ReactGA.send({ hitType: 'pageview', page });
+};
+
+export const registerFilterSearchEvent = (
+  filter_type: string,
+  filter_value: string
+) => {
+  ReactGA.event('filter-search', {
+    filter_type,
+    filter_value
+  });
+};
